5e024b731d606cc2a3f6a623303e8acc978a578f
Achraf-Git-dev <78796647+Achraf-Git-dev@users.noreply.github.com>
2023-02-27 23:40:05 +0100

    Update LoginTest.cs
    headless option added

--- src/AzureIoTHub.Portal.Tests.E2E/LoginTest.cs
+++ src/AzureIoTHub.Portal.Tests.E2E/LoginTest.cs
@@ -19,6 +19,7 @@ namespace AzureIoTHub.Portal.Tests.E2E
             ChromeOptions options = new ChromeOptions();
             options.AddArgument("--no-sandbox");
             options.AddArgument("--disable-dev-shm-usage");
+            options.AddArgument("headless");
             driver = new ChromeDriver("/usr/local/bin", options);
         }
 
